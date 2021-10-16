a7e4d2d12b3838d84ab8eca8b941089494e7a875
aegan123 <juhani@fmail.co.uk>
2021-10-16 16:33:46 +0300

    Initial Commit

---
+++ src/main/java/fi/asteriski/ilmot/IlmotApplication.java
@@ -0,0 +1,12 @@
+package fi.asteriski.ilmot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlmotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlmotApplication.class, args);
+    }
+}
---
+++ src/test/java/fi/asteriski/ilmot/IlmotApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.asteriski.ilmot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlmotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
